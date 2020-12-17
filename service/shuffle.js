062261c4b6be430096354658bd862546c2f12bce
Hardanonymous <hard@microsys.com.tw>
2020-12-17 23:45:10 +0800

    update

--- service/shuffle.js
+++ service/shuffle.js
@@ -1,3 +1,4 @@
+const { format } = require('date-fns');
 const db = require('../repository/firebase');
 
 function shuffle(users) {
@@ -9,13 +10,18 @@ function getRandom(min, max) {
 }
 
 async function shuffleGift() {
+  console.clear();
   const users = await db.user.read();
   const userContainer = users.map((user) => user.userId);
-
+  console.log(`--------------------${format(Date.now(), 'yyyy-MM-dd HH:mm:ss')}--------------------`);
   const shuffleUsers = shuffle(users);
   const giftsTmp = shuffleUsers.map((user, index) => {
+    console.log('userContainer', userContainer);
+    console.log('----------------------------------------');
     const userTmpContainer = userContainer
       .filter((userTmp) => userTmp !== user.userId);
+    console.log('userTmpContainer', userTmpContainer);
+
     const toIndex = getRandom(0, userTmpContainer.length - 1);
     const toUserId = userTmpContainer[toIndex];
     userContainer.splice(userContainer.indexOf(toUserId), 1);
@@ -25,17 +31,23 @@ async function shuffleGift() {
       to: toUserId,
     };
   });
+  console.log('----------------------------------------');
+  console.log('giftsTmp', giftsTmp);
+  console.log('----------------------------------------');
 
+  // "Cannot destructure property 'ownerGift' of 'giftsTmp.find(...)' as it is undefined."
   const gifts = giftsTmp.map((gift) => {
     const { ownerGift } = giftsTmp.find((tmp) => gift.to === tmp.owner);
-    console.log('test', ownerGift);
-    return db.gift.create({
+    return {
       ...gift,
       toGift: ownerGift,
-    });
+    };
+    // return db.gift.create();
   });
 
-  await Promise.all(gifts);
+  console.log(gifts);
+
+  // await Promise.all(gifts);
 }
 
 async function shuffleResult() {
